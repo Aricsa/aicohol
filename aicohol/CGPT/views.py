6e3255c5717dd87a5dffa6ceb22b8ab8b9487d4e
aricsa <topww00@naver.com>
2024-05-31 20:57:22 +0900

    data.json 추가 및 변경, 프롬프트 추가

--- aicohol/CGPT/views.py
+++ aicohol/CGPT/views.py
@@ -14,8 +14,19 @@ def get_completion(prompt):
         try:
             print(prompt)
             query = client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": prompt}],
+                model="gpt-3.5-turbo-0613",
+                messages=[
+                    {
+                        "role": "assistant",
+                        "content": '다음은 술을 마시러 온 손님과 술을 추천해주는 바텐더와의 대화입니다.\
+                            1. 손님이 말하는 문장에 "술 추천"이라는 단어가 있다면 바텐더는 주어지는 술 데이터를 참조하여 질문에 대해 답변해야 합니다.\
+                            2. 손님이 말하는 문장에 "지금까지 추천받은 술을 모두 말해줘"와 같거나 비슷한 내용이 포함된다면 바텐더는 주어지는 술 데이터를 참조하여 질문에 대해 답변해야합니다.\
+                            3. 손님이 말하는 문장에 "추천받은 술이랑 비슷한 술을 추천해줘"와 같거나 비슷한 내용이 포함된다면 바텐더는 주어지는 술 데이터를 참조하여 질문에 대해 답변해야합니다.\
+                            그 외의 질문은 바텐더가 보통 사람들을 대하는 것처럼 대화합니다.\
+                            손님 : '
+                        + prompt,
+                    }
+                ],
                 max_tokens=1024,
                 n=1,
                 stop=None,
@@ -37,5 +48,6 @@ def query_view(request):
         return JsonResponse({"response": response})
     return render(request, "index.html")
 
+
 def category_view(request):
-    return render(request, 'category.html')
+    return render(request, "category.html")
